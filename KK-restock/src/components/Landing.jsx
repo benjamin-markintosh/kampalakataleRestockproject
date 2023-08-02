d031f61198d21668a18f11aab74a31e8af2ab4a9
Benjamin Markintosh <benjaminokiza@gmail.com>
2023-08-02 11:17:15 +0300

    Added my new branch and also worked on somecomponents. the Nav barand Landing page

--- KK-restock/src/components/Landing.jsx
+++ KK-restock/src/components/Landing.jsx
@@ -1,30 +1,9 @@
+import NavDesktop from "./NavDesktop";
+
 function Landing() {
   return (
     <>
-      <div className="w-100 min-h-12 bg-kkblack ">
-        <div className="nav flex flex-wrap flex-row">
-          <div className="text-kkyellow p-3 flex-1">
-            <span className="text-kkwhite ">Kampala</span>Katale
-          </div>
-          <div className="menu text-kkwhite ">
-            <ul className="flex flex-row flex-wrap p-3">
-              <a href="">
-                <li>Home</li>
-              </a>
-              <a href="">
-                <li>Home</li>
-              </a>
-              <a href="">
-                <li>Home</li>
-              </a>
-              <a href="">
-                <li>Home</li>
-              </a>
-            </ul>
-          </div>
-        </div>
-      </div>
-
+      <NavDesktop />
       <div className="body-section">
         <div className="ryt"></div>
         <div className="lft">
