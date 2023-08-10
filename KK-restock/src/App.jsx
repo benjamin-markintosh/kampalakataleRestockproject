da9684327185a64e2053f409b085bcc462c18587
kamu-mavin-baguma2 <kamurashvin@gmail.com>
2023-08-11 01:05:15 +0400

    have created product cart components and images and carousel

--- KK-restock/src/App.jsx
+++ KK-restock/src/App.jsx
@@ -1,14 +1,18 @@
 //import "./App.css";
 
-// import NavAdmin from "./components/NavAdmin";
 
 
 
 
-// import { Card } from "@mantine/core";
+
+
+
 
 function App() {
-  return <>Hello World</>;
+  return <>
+    
+    
+  </>;
 }
 
 export default App;
