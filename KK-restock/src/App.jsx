7a8a6e8a3a18f9500424cdeb13d435f854d0139e
Benjamin Markintosh <benjaminokiza@gmail.com>
2023-08-17 06:16:45 +0300

    Worked on the Products Suppliers layout page

--- KK-restock/src/App.jsx
+++ KK-restock/src/App.jsx
@@ -1,17 +1,7 @@
-
 //import "./App.css";
-
-
-
-
-
-
-
-
 
 function App() {
   return <></>;
-
 }
 
 export default App;
--- KK-restock/src/components/Landing.jsx
+++ KK-restock/src/components/Landing.jsx
@@ -1,22 +1,29 @@
-import React from "react";
-import '../App.css'
-
+import "../App.css";
 
 function Landing() {
   return (
-    <div className="container">
-      <div className="main-text ">
-        <div className="">
-          <p className="para text-gray-500">Are you a Retailer or a Wholeseller?</p>
+    <div className="h-screen flex justify-center sm:justify-end items-center mx-32">
+      {/* <div className="main-text "> */}
+      <div className="">
+        <div className="text-kkblack w-[256px] text-2xl sm:text-4xl font-normal sm:w-[392px] text-left  pb-4">
+          {/* <p className="para text-gray-500"> */}
+          <p className=" ">
+            Are you a Retailer or <br /> a Wholeseller?
+          </p>
         </div>
-        <div className="para2 font-bold">
-          <h2 className="">Katale with us...</h2>
+        {/* <div className="para2 font-bold"> */}
+        <div className=" text-kkblack w-[256px] text-4xl sm:text-8xl font-bold  sm:w-[392px] text-left pb-4">
+          <h2 className="">
+            Katale <br /> with us...
+          </h2>
         </div>
-        <div className="btn">
-          <button className="">REGISTER NOW</button>
+        <div className="w-44 h-8 sm:w-72  sm:h-16 bg-kkorange rounded-full flex item-center justify-center">
+          <button className="text-white text-lg  sm:text-2xl font-bold">
+            REGISTER NOW
+          </button>
         </div>
       </div>
-     
+
       {/*<div className="footer">
       
       </div>*/}
